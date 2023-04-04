443aa57d0e480c7a8fc5919cb150a85b2706b15c
a1822939 <a1822939@student.adelaide.edu.au>
2023-04-04 21:35:25 +0930

    v1

--- Finder.cpp
+++ Finder.cpp
@@ -1,20 +1,27 @@
 #include "Finder.h"
 
-    using namespace std;
+using namespace std;
 
-    vector<int> Finder::findSubstrings(string s1, string s2) {
+vector<int> Finder::findSubstrings(string s1, string s2)
+{
 
-        vector<int> result;
+    vector<int> result;
+    int startIndex = 0;
 
-        for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
-            if (found != string::npos) {
-                result.push_back(found);
-            } else {
+    for (size_t i = 1; i <= s2.size(); i++)
+    {
+        size_t found = s1.find(s2.substr(0, i), startIndex);
+        if (found != string::npos)
+        {
+            result.push_back(found);
+            startIndex = found;
+        }
+        else
+        {
+            for (size_t k = i; k <= s2.size(); k++)
+            {
                 result.push_back(-1);
             }
         }
         return result;
     }
-
-   
